c2e91f752decec9ec00abed4002260fb0fabc663
Noah Nethery <nethery.n@gmail.com>
2020-05-15 00:24:56 -0500

    Finished sprites

--- sprites/bucket.py
+++ sprites/bucket.py
@@ -1,23 +1,59 @@
 import pygame
-image_file = '/Users/noah/Documents/pygame/images/bucket.png'
+image_file = 'images/bucket.png'
+pixel_distance = 5
 
 class Bucket(pygame.sprite.Sprite):
-    def __init__(self, image_file = image_file, location = (50,50)):
+    def __init__(self, image_file:str = image_file):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_file)
-        self.x, self.y = location
-        # self.rect = pygame.image.get_rect()
-        # self.rect.top, self.rect.left = location
-        self.pixel_distance = 5
-        print(self.image)
+        self.rect = self.image.get_rect()
+        self.can_move = False
 
-    def draw(self, surface):
+    def draw(self, surface:pygame.Surface):
         surface.blit(self.image, (self.x, self.y))
 
     def handle_keys(self):
         key = pygame.key.get_pressed()
-        
         if key[pygame.K_RIGHT]:
-            self.x += self.pixel_distance
+            self.move(1)
         elif key[pygame.K_LEFT]:
-            self.x -= self.pixel_distance
+            self.move(0)
+
+    def move(self, direction, distance = pixel_distance):
+        if direction == 1:
+            self.x = self.x + distance if self.x + distance < 800 else 800
+        else:
+            self.x = self.x - distance if self.x - distance > 0 else 0
+        self.keep_in_bounds()
+        self.rect.topleft = (self.x, self.y)
+
+    def keep_in_bounds(self):
+        if self.x < 0:
+            self.x = 0
+        elif self.x > 800 - self.rect.width:
+            self.x = 800 - self.rect.width
+
+    def check_for_move(self):
+        mouse = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(mouse):
+            self.can_move = True
+
+        if self.can_move and pygame.mouse.get_pressed()[0]:
+            if self.mouse_distance == None:
+                self.mouse_distance = mouse[0] - self.x
+            if self.lastX == None:
+                self.lastX = mouse[0]
+            else:
+                self.x = mouse[0] - self.mouse_distance
+        else:
+            self.mouse_distance = None
+            self.lastX = None
+            self.can_move = False
+        
+        self.keep_in_bounds()
+        self.rect.topleft = (self.x, self.y)
+    
+    def set_location(self, location):
+        self.x, self.y = location
+        self.rect.topleft = (self.x, self.y)
+        self.lastX = self.x
--- sprites/raindrop.py
+++ sprites/raindrop.py
@@ -1,13 +1,23 @@
 import pygame
+import random
+image_files = ['images/droplet.png', 'images/bad droplet.png']
 
 class Raindrop(pygame.sprite.Sprite):
-    def __init__(self, image_file, left, speed):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.rect = pygame.image.get_rect()
-        self.rect.top = 50
-        self.rect.left = left
-        self.speed = speed
+    speed_base = 200
 
-    def move(self):
-        self.rect = self.rect.move(self.speed)
+    def __init__(self, speed:int = 0, image_files:list = image_files, location:tuple = (50,50)):
+        pygame.sprite.Sprite.__init__(self)
+        self.is_bad = True if random.randrange(0,6) == 0 else False
+        self.image = pygame.image.load(image_files[1]) if self.is_bad else pygame.image.load(image_files[0])
+        self.rect = self.image.get_rect()
+        self.y = 0 - self.rect.height
+        self.x = random.randint(0, 800 - self.rect.width)
+        self.rect.topleft = (self.x, self.y)
+        self.speed = random.randint(Raindrop.speed_base, Raindrop.speed_base + 100)
+
+    def fall(self):
+        self.y += (self.speed) * (1/60)
+        self.rect.topleft = (self.x, self.y)
+
+    def draw(self, surface:pygame.Surface):
+        surface.blit(self.image, (self.x, self.y))
